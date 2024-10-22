fb73b68d28fbc46087e80849334a276f72a400cc
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2024-10-22 20:40:12 +0300

    feat: added Categories, Roles, Users, Profiles

---
+++ src/Domain/Categories/Category.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Domain.Categories;
+
+public class Category
+{
+    public CategoryId Id { get; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    private Category(CategoryId id, string name, string description)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+    }
+    
+    public static Category New(CategoryId id, string name, string description)
+        => new(id, name, description);
+
+    public void UpdateDetails(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
