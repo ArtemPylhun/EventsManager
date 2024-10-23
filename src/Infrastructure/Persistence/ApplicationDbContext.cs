1ba9b4d3a95671a116cf49b15f16b3557e4e4987
Artem Pylhun <artem.pylhun@oa.edu.ua>
2024-10-23 20:02:47 +0300

    feat: Infrastructure and Application project set up

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Domain.Attendances;
+using Domain.Categories;
+using Domain.Events;
+using Domain.EventsTags;
+using Domain.Locations;
+using Domain.Profiles;
+using Domain.Roles;
+using Domain.Tags;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Attendance> Attendances { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<EventTag> EventsTags { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
