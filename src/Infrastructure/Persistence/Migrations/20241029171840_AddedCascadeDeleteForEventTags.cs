1562a162dc5915351e9a37c516145c49eb429dba
MaksymTomusiak <maksym.tomusiak@oa.edu.ua>
2024-10-29 20:41:05 +0200

    feat: add cascade delete for join table

---
+++ src/Infrastructure/Persistence/Migrations/20241029171840_AddedCascadeDeleteForEventTags.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCascadeDeleteForEventTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_events_tags_events_id",
+                table: "events_tags");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_events_tags_tags_id",
+                table: "events_tags");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_events_tags_events_event_id",
+                table: "events_tags",
+                column: "event_id",
+                principalTable: "events",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_events_tags_tags_tag_id",
+                table: "events_tags",
+                column: "tag_id",
+                principalTable: "tags",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_events_tags_events_event_id",
+                table: "events_tags");
+
+            migrationBuilder.DropForeignKey(
+                name: "fk_events_tags_tags_tag_id",
+                table: "events_tags");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_events_tags_events_id",
+                table: "events_tags",
+                column: "event_id",
+                principalTable: "events",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_events_tags_tags_id",
+                table: "events_tags",
+                column: "tag_id",
+                principalTable: "tags",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
